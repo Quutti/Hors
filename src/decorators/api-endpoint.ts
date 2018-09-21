e16d66dda4cf7b03f748cb600d1b1ab20a721ee8
Jokke Liikkanen <quutticode@gmail.com>
2018-09-21 22:09:21 +0300

    Add support for adding middleware for endpoints

--- src/decorators/api-endpoint.ts
+++ src/decorators/api-endpoint.ts
@@ -1,13 +1,15 @@
 import 'reflect-metadata';
 
 import { ENDPOINTS_METADATA_SYMBOL } from '../metadata';
+import { EndpointMiddleware } from '../transaction/transaction-utils';
 
 export type ApiHttpMethod = 'get' | 'post' | 'put' | 'patch' | 'delete';
 
 export interface StoredEndpoint {
     url: string;
     method: ApiHttpMethod;
-    publicEndpoint: boolean;
+    public: boolean;
+    middleware: EndpointMiddleware[];
     target: any;
 }
 
@@ -15,20 +17,29 @@ export interface ApiEndpointSettings {
     method: ApiHttpMethod;
     url: string;
     public?: boolean;
+    middleware?: EndpointMiddleware[];
 }
 
 export const apiEndpoint = (settings: ApiEndpointSettings) => {
     return (target: any) => {
-        const { method, url, public: publicEndpoint } = settings;
+        const { method, url, public: isPublic } = settings;
+        const middleware = settings.middleware || [];
         const endpoints: StoredEndpoint[] = Reflect.getMetadata(ENDPOINTS_METADATA_SYMBOL, Reflect) || [];
 
+        // Protect against duplicate endpoints
         endpoints.forEach(endpoint => {
             if (endpoint.method === method && endpoint.url === url) {
                 throw new Error(`Duplicate endpoint registered (${method.toLocaleUpperCase()} ${url})`);
             }
         });
 
-        endpoints.push({ url, method, publicEndpoint, target });
+        endpoints.push({
+            url,
+            method,
+            target,
+            middleware,
+            public: isPublic
+        });
 
         Reflect.defineMetadata(ENDPOINTS_METADATA_SYMBOL, endpoints, Reflect);
     }
